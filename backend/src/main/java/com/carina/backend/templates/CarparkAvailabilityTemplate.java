9bc55b839242ed1156edbe0cee6ab65c15b7865a
nwjbrandon <nwjbrandon@gmail.com>
2019-09-12 23:24:29 +0800

    enabled caching

--- backend/src/main/java/com/carina/backend/templates/CarparkAvailabilityTemplate.java
+++ backend/src/main/java/com/carina/backend/templates/CarparkAvailabilityTemplate.java
@@ -2,6 +2,7 @@ package com.carina.backend.templates;
 
 import com.carina.backend.services.CarparkAvailability;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -20,6 +21,8 @@ public class CarparkAvailabilityTemplate {
         this.mongoTemplate = mongoTemplate;
     }
 
+    // https://spring.io/guides/gs/caching/
+    @Cacheable("Carpark Availability")
     public List<CarparkAvailability> findByArea(String area) {
         Query query = new Query();
         query.addCriteria(Criteria.where("Area").is(area));
