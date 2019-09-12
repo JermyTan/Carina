b494f2783423b97766d6957bbcfc32772b9f0d58
nwjbrandon <nwjbrandon@gmail.com>
2019-09-12 23:15:05 +0800

    added comments for the template database

--- backend/src/main/java/com/carina/backend/templates/CarparkAvailabilityTemplate.java
+++ backend/src/main/java/com/carina/backend/templates/CarparkAvailabilityTemplate.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+// http://appsdeveloperblog.com/spring-boot-and-mongotemplate-tutorial-with-mongodb/
 @Repository
 public class CarparkAvailabilityTemplate {
 
