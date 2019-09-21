f2c6c2142160f0bd5d43432ab96c4724aacd14e3
nwjbrandon <nwjbrandon@gmail.com>
2019-09-21 21:30:31 +0800

    refactor service

--- backend/src/main/java/com/carina/app/service/CarparkAvailabilityService.java
+++ backend/src/main/java/com/carina/app/service/CarparkAvailabilityService.java
@@ -1,20 +1,11 @@
 package com.carina.app.service;
 
 import com.carina.app.model.CarparkAvailabilityMondayModel;
-import com.carina.app.repository.CarparkAvailabilityMondayRepository;
-import com.carina.app.template.CarparkAvailabilityMondayTemplate;
 import com.carina.app.utilities.DistanceCalculationService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 
 public class CarparkAvailabilityService {
-
-    @Autowired
-    private CarparkAvailabilityMondayRepository carparkAvailabilityMondayRepository;
-
-    @Autowired
-    private CarparkAvailabilityMondayTemplate carparkAvailabilityMondayTemplate;
 
     public static ArrayList<CarparkAvailabilityMondayModel> getNearestCarpark(
             double latitudeSource, double longitudeSource, double radiusProximity, ArrayList<CarparkAvailabilityMondayModel> listOfCarparkAvailabilityMondayModels
