853d252da4ba71d9c188085e0c0bba677894a532
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-09-23 03:22:58 +0800

    Set up firebase

---
+++ frontend/carina/src/firebase.tsx
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyBWtaENcaZXFCHyUN6TphD2UsQ3IGWmr7w",
+  authDomain: "carina-cc844.firebaseapp.com",
+  databaseURL: "https://carina-cc844.firebaseio.com",
+  projectId: "carina-cc844",
+  storageBucket: "",
+  messagingSenderId: "710268688109",
+  appId: "1:710268688109:web:db444b56047046da365ee4"
+};
+
+firebase.initializeApp(config);
+export default firebase;
