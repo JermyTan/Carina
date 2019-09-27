3ed03a28756927b4f267d66b4a160d0087396979
Jeremy <tan_kai_qun97@yahoo.com.sg>
2019-09-27 20:31:16 +0800

    Add check for online/offline status

--- frontend/carina/src/App.tsx
+++ frontend/carina/src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import MainPage from "./pages/Main";
 import OfflinePage from "./pages/Offline";
+//import Axios from "axios";
 
 interface IAppState {
   isOnline: boolean;
+  isLoading: boolean;
 }
 
 class App extends React.Component<any, IAppState> {
@@ -11,16 +13,46 @@ class App extends React.Component<any, IAppState> {
     super(props);
 
     this.state = {
-      isOnline: navigator.onLine
+      isOnline: navigator.onLine,
+      isLoading: true
     };
+
+    this.handleConnection = this.handleConnection.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("online", this.handleConnection);
+    window.addEventListener("offline", this.handleConnection);
+    this.handleConnection();
+  }
+
+  handleConnection() {
+    if (navigator.onLine) {
+      fetch(`${process.env.REACT_APP_BACKEND_API}public/version`, {
+        method: "HEAD",
+        mode: "no-cors"
+      })
+        .then((response: any) => {
+          return response && (response.ok || response.type == "opaque");
+        })
+        .then((isOnline: boolean) => {
+          this.setState({ isOnline, isLoading: false });
+        });
+    } else {
+      this.setState({ isOnline: false, isLoading: false });
+    }
+  }
+
+  renderPage() {
+    if (this.state.isLoading) {
+      return <div />;
+    } else {
+      return this.state.isOnline ? <MainPage /> : <OfflinePage />;
+    }
   }
 
   render() {
-    return (
-      <div className="App">
-        {this.state.isOnline ? <MainPage /> : <OfflinePage />}
-      </div>
-    );
+    return <div className="App">{this.renderPage()}</div>;
   }
 }
 
